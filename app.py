2a60ab010bec38e7944a2e799443c11f57d4bc98
nancycao <ncao@stuy.edu>
2017-02-18 14:36:44 -0500

    changed

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def webhook():
 
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
-                print messaging event
+                print messaging_event
                 if messaging_event.get("message"):  # someone sent us a message
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
