1c9054ca1072302a1198fd3b4fb81d69580bcd73
nancycao <ncao@stuy.edu>
2017-02-18 22:06:00 -0500

    more testing

--- app.py
+++ app.py
@@ -108,13 +108,14 @@ def webhook():
                         send_message(sender_id, "You are a volunteer")
                     elif action == "CLIENT":
                         USER = "CLIENT"
-                        tempDict = { }
-                        tempDict['id'] = sender_id
-                        tempDict['focus'] = 'N/A'
-                        tempDict['currState'] = 'N/A'
-                        tempDict['age'] = 0
+                        send_message(sender_id, "You are a client")
+                        #tempDict = { }
+                        #tempDict['id'] = sender_id
+                        #tempDict['focus'] = 'N/A'
+                        #tempDict['currState'] = 'N/A'
+                        #tempDict['age'] = 0
                         #db.addClient( {'id':sender_id, 'age' : 0, 'focus' : 'N/A', 'currState' : 'N/A'} )
-                        db.addClient( tempDict )
+                        #db.addClient( tempDict )
                         send_message(sender_id, "Created client")
                         send_categories(sender_id)
                     elif action == "IMMIGRATION_LAW" or action == "CITIZENSHIP" or action == "VISA":
