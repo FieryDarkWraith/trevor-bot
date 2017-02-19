c269b1ead99cbbb0ab3a3b0273b86074077c6d9b
nancycao <ncao@stuy.edu>
2017-02-18 22:36:29 -0500

    Something

---
+++ test.py
@@ -0,0 +1,21 @@
+from utils import db
+
+info = {}
+info['id'] = "kevin"
+info['currState'] = "New York"
+info['age'] = 18
+info['focus'] = "Feeling Happy"
+
+#db.addClient( info )
+
+info2 = {}
+info2['id'] = "bleh"
+info2['currState'] = "New York"
+info2['name'] = "Celine"
+
+#db.addLawyer( info2 )
+
+
+#db.unpair( "kevin", "bleh" )
+
+print db.findMatchingId("bleh")
